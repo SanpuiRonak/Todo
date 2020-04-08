9dbd5c3c4bacd79878a62a559c42a042662de258
SanpuiRonak <ronaksanpui@gmail.com>
2020-04-08 18:04:51 +0530

    Progrress 6.04

--- Todo.java
+++ Todo.java
@@ -3,10 +3,32 @@ import java.util.*;
 class Todo {
     public static void main(String[] arg) {
         Map<Integer, Task> m = new HashMap<>();
-        String s = "Heloo";
         
-        
-        
+
+    }
+
+    String getCommand(String s)
+    {
+        if(s.equals("/list"))
+        {
+            return s;
+        }
+        else if(s.substring(0,10).equals("/list add"))
+        {
+            return "/list add";
+        }
+        else if(s.substring(0,12).equals("/list delete"))
+        {
+            return "/list delete";
+        }
+        else if(s.substring(0,13).equals("/list rename"))
+        {
+            return "/list rename";
+        }
+        else
+        {
+            return "";
+        }
     }
 
     
